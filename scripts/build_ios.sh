2a582616a60ab67886fc3eac7b7b713084c594da
Mushaheed Syed <mushaheed@reclaimprotocol.org>
2025-09-25 01:40:43 +0530

    Patch privacymanifest (#28)
    * Remove pubspec.lock
    
    * Add script for using privacy analyze tool

--- scripts/build_ios.sh
+++ scripts/build_ios.sh
@@ -6,8 +6,9 @@ export VERSION=$(grep '^version:' pubspec.yaml | sed -e 's/version: //')
 
 DIST_IOS=./dist/ios/$VERSION
 
-(cd .ios && pod deintegrate;)
+(cd .ios && pod deintegrate && rm -rf Podfile.lock && rm -rf ./Pods/);
 sed -i '' "s/platform :ios, '.*'/platform :ios, '14.0'/" ./.ios/Podfile;
+
 (cd .ios && pod install)
 mkdir -p build/ios
 mkdir -p debug/ios/
